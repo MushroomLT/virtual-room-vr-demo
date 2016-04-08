46f1e081e5fe1e67bc2753963ee2d6c638bd880e
MushroomLT <kasparas6@hotmail.com>
2016-04-09 00:40:38 +0300

    Basic networking implementation

---
+++ Assets/Scripts/Net/SmoothPositionSync.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SmoothPositionSync : NetworkBehaviour {
+
+    [SyncVar]
+    public Vector3 networkPosition;
+
+    public int smoothFactor = 8;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isServer) {
+            networkPosition = transform.position;
+            return;
+        }
+        transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * smoothFactor);
+    }
+}
